fd91a5eb66155b36f09018d2a04031bad800a130
gabriele agosta <g.agosta02@gmail.com>
2022-08-23 16:27:25 +0200

    Added functionality that allows the user to choose the amout of squares

--- scripts.js
+++ scripts.js
@@ -1,12 +1,40 @@
 const container = document.querySelector('#container');
-const newLine = document.createElement('br')
+const button = document.querySelector('button')
+const width = container.offsetWidth;
 
-function getGrid(userSelection){
+
+button.addEventListener('click', getCustomGrid)
+
+function deleteOldGrid(){
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+function getCustomGrid(){
+    let userSelection = 0;
+    do{
+        userSelection = parseInt(window.prompt('Enter the number of squares (max. 100): '));
+    }while (userSelection < 0 || userSelection > 100);
+    deleteOldGrid();
+    getGrid(userSelection);
+}
+
+
+function pressedSquare(e){
+    this.classList.toggle('pressed');
+}
+
+
+function getGrid(userSelection=16){
+    squareSize = width / userSelection;
     for(let i=0; i<userSelection; i++){
         let row = document.createElement('div');
         for(let j=0; j<userSelection; j++){
             let div = document.createElement('div');
-            div.setAttribute('class', 'square');
+            div.setAttribute('style', `border:1px solid black; width:${squareSize}px; height:${squareSize}px`);
+            div.addEventListener('click', pressedSquare)
             row.appendChild(div)
         }
         container.appendChild(row);
@@ -14,7 +42,4 @@ function getGrid(userSelection){
 }
 
 
-const divs = document.querySelectorAll('.squares');
-divs.forEach(div => {
-    div.addEventListener('click', div.classList.add('pressed'));
-})
+getGrid();
